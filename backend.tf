d0bc7c96235aa479bad71c501639f273efba63d2
Nibia <nibiabotsi@gmail.com>
2024-07-28 17:53:11 -0400

    terraformweek10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-ks-terraform-ok" #replace with your bucket
+    bucket         = "fgfhdhdhg" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "yfyfdfud" #replace with the table
   }
 }
